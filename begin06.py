48a91a55f15ce9ea327cfa982e22cb82ee642f19
Bahtee <bahtiyorjon_89@mail.ru>
2022-12-19 00:40:05 +0500

    ozgaruvchi6

--- begin06.py
+++ begin06.py
@@ -7,3 +7,9 @@
 # Assign the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin06' to variable 'k';
 
 # # Print the variable 'k'.
+
+a = 24
+b = 3
+c = 5
+k = "https://github.com/codeschool-2022A/variables_and_types#begin06"
+print(k)
